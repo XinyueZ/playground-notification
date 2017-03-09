6a5cf8a8228bcc3fe0b8bf28c9f3352bcf5a5430
Chris Zhao <xinyue.zhao@immonet.de>
2017-03-09 10:10:17 +0100

    Removed savedInstanceState from initView

--- app/src/main/java/com/playground/notification/app/fragments/PlaygroundDetailFragment.java
+++ app/src/main/java/com/playground/notification/app/fragments/PlaygroundDetailFragment.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.BottomSheetDialogFragment;
@@ -299,7 +298,7 @@ public final class PlaygroundDetailFragment extends BottomSheetDialogFragment im
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		initView(savedInstanceState);
+		initView();
 	}
 
 	@NonNull
@@ -315,7 +314,7 @@ public final class PlaygroundDetailFragment extends BottomSheetDialogFragment im
 		return dialog;
 	}
 
-	private void initView(@Nullable Bundle savedInstanceState) {
+	private void initView() {
 		Bundle args = getArguments();
 		final Playground playground = (Playground) args.getSerializable(EXTRAS_GROUND);
 		if (playground != null) {
